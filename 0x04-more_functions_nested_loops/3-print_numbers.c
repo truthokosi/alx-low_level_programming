dfed27d6d60e5eca0f33e65b5406fe826729c8b1
truthokosi <okositruth0808@gmail.com>
2023-04-28 10:34:24 -0700

    Print single digits 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  *print_numbers - for printing numbers
+  *Return - Always 0 for success
+  */
+
+void print_numbers(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+{
+putchar(i);
+}
+putchar('\n');
+}
