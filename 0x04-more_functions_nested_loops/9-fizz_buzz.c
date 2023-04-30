aebda9ce0103aab38bf9785fd7745d430bba26b1
truthokosi <okositruth0808@gmail.com>
2023-04-30 12:35:52 -0700

    FizzBuzz multiples of 3, 5, and 15

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  *Description: Print numbers 1 to 100
+  *Fizz for multiples of 3, Buzz for multiples of 5 and FizzBuzz for multiples of 15
+  */
+
+int main(void)
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+if (i % 15 == 0)
+printf("FizzBuzz");
+else if (i % 5 == 0)
+printf("Buzz");
+else if (i % 3 == 0)
+printf("Fizz");
+else
+printf("%d", i);
+if (i < 100)
+printf(" ");
+}
+printf("\n");
+return (0);
+}
+
