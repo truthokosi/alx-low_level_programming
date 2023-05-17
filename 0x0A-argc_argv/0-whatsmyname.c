1f341461f1a13286247666655893a7a67764f083
truthokosi <okositruth0808@gmail.com>
2023-05-17 08:26:00 -0700

    Write a program that prints its name, if changed, without having to compile again, would print its new name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  *main - entry point
+  *@argc: argument count
+  *@argv: argument vector for array size
+  *Return: Always 0 for success
+  */
+
+int main(int argc, char *argv[])
+{
+(void) argc;
+printf("%s\n", argv[0]);
+
+return (0);
+}
