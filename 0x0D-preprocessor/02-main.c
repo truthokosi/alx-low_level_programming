2625276df165f3e880bd9331a55b09141601b11e
truthokosi <okositruth0808@gmail.com>
2023-05-22 15:49:46 -0700

    Creating macros in C

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+  *main - Entry point, prints the name of the file a
+  * program was compiled from
+  *Return: Always 0 for success
+  */
+
+int main(void)
+{
+printf("%s\n", __FILE__);
+
+return (0);
+}
---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,6 @@
+#ifndef MY_PI_H
+#define MY_PI_H
+
+#define PI 3.14159265359
+
+#endif
