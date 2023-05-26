0dc9e5bd371fc01c33741334867fe7de9e3617b0
truthokosi <okositruth0808@gmail.com>
2023-05-26 06:30:02 -0700

    Print all formats

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -5,7 +5,43 @@
   *@format: is a list of types of arguments passed to the function
   */
 
-void print_all(const char * const format, ...);
+void print_all(const char * const format, ...)
 {
 	int t = 0;
-char 
+	char *s, *p = "";
+
+	va_list ap;
+
+	va_start(ap, format);
+
+	if (format)
+	{
+		while (format[t])
+		{
+			switch (format[t])
+			{
+				case 'c':
+					printf("%s%c", p, va_arg(ap, int));
+					break;
+				case 'i':
+					printf("%s%d", p, va_arg(ap, int));
+					break;
+				case 'f':
+					printf("%s%f", p, va_arg(ap, double));
+					break;
+				case 's':
+					s = va_arg(ap, char *);
+					if (!s)
+						s = "(nil)";
+					printf("%s%s", p, s);
+					break;
+				default:
+					t++;
+					continue;
+			}
+			p = ", ";
+			t++;
+		}
+	}
+	printf("\n");
+}
