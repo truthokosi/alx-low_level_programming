153a6d09de95d5f67dba747b2ceb09bc549f91df
truthokosi <okositruth0808@gmail.com>
2023-05-26 06:12:37 -0700

    Print strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -12,6 +12,12 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	char *str;
 	va_list ap;
 
+	for (t = 0; t < n; t++)
+	{
+		if (separator == NULL)
+		continue;
+	}
+
 	va_start(ap, n);
 
 	for (t = 0; t < n; t++)
---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,11 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+  *print_all - a function that prints anything
+  *@format: is a list of types of arguments passed to the function
+  */
+
+void print_all(const char * const format, ...);
+{
+	int t = 0;
+char 
